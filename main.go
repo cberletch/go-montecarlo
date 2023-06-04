44fc9afdd41af841a268358fcba2cc5641089400
Cole <coleberletch@gmail.com>
2023-06-04 13:30:46 -0400

    #2

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 const life_expectancy = 85
 
-func GetInput(x string) int {
+func GetInt(x string) int {
 	var val int
 
 	fmt.Println("Enter", x, ":")
@@ -27,31 +27,85 @@ func GetFloat(x string) float32 {
 	return val
 }
 
+func GetRisk() (float32, float32) {
+	var input int
+	var mean float32
+	var stddev float32
+
+	fmt.Println("Select risk level from the below options:")
+	fmt.Println("1. Aggresive - can accept losses upwards of 40%")
+	fmt.Println("2. Semi-aggresive - can accept losses upwards of 25%")
+	fmt.Println("3. Neutral - can accept losses upwards of 15%")
+	fmt.Println("4. Semi-averse - can accept losses upwards of 10%")
+	fmt.Println("5. Risk-averse - can accept losses upwards of 5% ")
+
+	for {
+		fmt.Print("Enter a number between 1 and 5: ")
+		_, err := fmt.Scan(&input)
+		if err != nil {
+			fmt.Println("Invalid input. Please Enter your selection as an integer between 1 and 5.")
+			continue
+		}
+		if input < 1 || input > 5 {
+			fmt.Println("Invalid input. Please Enter your selection as an integer between 1 and 5.")
+			continue
+		}
+		break
+	}
+
+	if input == 5 {
+		mean = 10.0
+		stddev = 17.9
+	}
+
+	fmt.Println(input)
+
+	return mean, stddev
+}
+
 func main() {
-	/*var age int
-	var assets int
+	var age int
+	var assets float32
 	var retirement_age int
 	var burn_rate float32
+	var mean float32
+	var stddev float32
 
-	age = GetInput("age")
+	age = 65 //GetInt("age")
 	fmt.Println(age)
 
-	assets = GetInput("assets")
+	mean, stddev = GetRisk()
+	fmt.Println(mean, stddev)
+
+	assets = 100000 //GetFloat("assets")
 	fmt.Println(assets)
 
-	retirement_age = GetInput("retirement age")
+	retirement_age = 65 //GetInt("retirement age")
 	fmt.Println(retirement_age)
 
-	burn_rate = GetFloat("burn rate")
-	fmt.Println(burn_rate)*/
+	burn_rate = .04 //GetFloat("burn rate")
+	fmt.Println(burn_rate)
 
-	for i := 0; i < 100; i++ {
+	for i := 0; i < life_expectancy-age; i++ {
+
+		// add logic to edit mean and std. dev based on risk tolerance later
+
+		if age >= retirement_age {
+
+			rand.Seed(time.Now().UnixNano() + int64(i))
+			randomFloat := rand.NormFloat64() * float64(stddev+mean)
+			randomFloat = math.Max(-38.5, math.Min(48.9, randomFloat))
+
+			assets = assets * (1.0 + float32(randomFloat/100.0))
+			fmt.Println(int(assets))
+		}
+
 		rand.Seed(time.Now().UnixNano() + int64(i))
-		mean := 0.0
-		stddev := 20.0
-		randomFloat := rand.NormFloat64()*stddev + mean
-		randomFloat = math.Max(-50, math.Min(50, randomFloat))
-		fmt.Println(randomFloat)
+
+		randomFloat := rand.NormFloat64() * float64(stddev+mean)
+		randomFloat = math.Max(-38.5, math.Min(48.9, randomFloat))
+		assets = assets * (1.0 + float32(randomFloat/100.0))
+		age++
 	}
 
 }
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Monte Carlo Financial Planning
+
+- DISCLAIMER: This is not financial advice. This application represents a purely mathematical simulation exercise. This application will be limited by resources available to accurately paint a picture of current capital markets. The undelying investment and tax features could be greatly improve with assistance and data from instituional investors and accounting professionals. It is recommended to consult with a licensed financial advisor when making financial decisions. 
+
+- This application aims to predict EOL (End of life) assets based on a variety of factors including:
+    - Stock market returns adjusted to a risk tolerance level
+    - Assets at time of entering
+    - Income in years prior to retirement
+    - Expected annual inflation
+    - Expected expendatures (housing, insurance, gifts, child expenses, vacations)
+    - Tax planning (to be added)
+
+- This application will initially be written in Golang as a learning exercise, but will likely be ported to Typescript in the future and converted to a web/mobile app.
